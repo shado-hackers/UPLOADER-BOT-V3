5fc6b229506c2139170575b1d6aec687717e4e0c
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-01-24 18:36:37 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -73,6 +73,8 @@ async def button(bot, update):
     elif "close" in update.data:
         await update.message.delete(True)
 
+    elif "close" in update.data:
+        await update.message.delete(True)
     elif "|" in update.data:
         await youtube_dl_call_back(bot, update)
     elif "=" in update.data:
